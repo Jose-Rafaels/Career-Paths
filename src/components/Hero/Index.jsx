dcb3d98a266fb7035f3daa3e1d61e22db22a8fb3
Dwi Sam <dwisamsiarto@gmail.com>
2023-01-31 22:32:41 +0700

    add hero & navbar

---
+++ src/components/Hero/Index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Styles.css";
+import hero from "../../assets/Hero.png";
+
+const Intro = () => {
+  return (
+    <div className="intro" id="Intro">
+      <div className="i-left">
+        <div className="i-name">
+          <p className="name">
+            Find the Path to Reach
+            <br />
+            Your Dream Jobs
+          </p>
+          <p className="i-info">
+            Yuk kenali potensi diri kamu yang harus kamu kembangkan
+            <br />
+            demi mencapai pekerjaan impian kamu.
+          </p>
+          <button>Get Started</button>
+        </div>
+      </div>
+      <div className="i-right">
+        <div className="i-box">
+          <div>
+            <img src={hero} alt="hero" height={594} width={524.49} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
