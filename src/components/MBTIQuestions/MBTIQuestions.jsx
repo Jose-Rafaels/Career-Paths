7c609a6c74cb0bc8c6a19a9e4cfe468da91f4d95
Dwi Sam <dwisamsiarto@gmail.com>
2023-03-20 11:23:17 +0700

    fixed progress bar

--- src/components/MBTIQuestions/MBTIQuestions.jsx
+++ src/components/MBTIQuestions/MBTIQuestions.jsx
@@ -1,19 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./Styles.css";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import Title from "../Layout/Title";
 
-const MBTIQuestion = ({ question, onAnswerSelect, onNextQuestion }) => {
+const MBTIQuestion = ({
+  question,
+  onAnswerSelect,
+  onNextQuestion,
+  totalQuestions,
+}) => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+
   const handleAnswerClick = (answerId) => {
     onAnswerSelect(question.id, answerId);
     onNextQuestion();
+    setCurrentQuestion(currentQuestion + 1);
   };
+
+  const progress = Math.ceil((currentQuestion / 52) * 100);
+
   return (
     <Title title="Test">
       <>
@@ -25,8 +36,10 @@ const MBTIQuestion = ({ question, onAnswerSelect, onNextQuestion }) => {
           </p>
 
           <div className="box-progress" data-aos="fade-up">
-            <p className="progress">Progress 8/10</p>
-            <ProgressBar animated now={50} />
+            <p className="progress">
+              Progress {currentQuestion}/{52}
+            </p>
+            <ProgressBar animated now={progress} label={`${progress}%`} />
           </div>
 
           <div className="box-question" data-aos="fade-up">
