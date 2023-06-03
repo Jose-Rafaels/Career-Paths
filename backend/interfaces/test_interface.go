1fa4c07e807ecc2eaf531872d50dfb5f42ea56b1
sautmanurung1 <smanurung360@gmail.com>
2023-06-03 20:44:40 +0700

    delete some source code

--- backend/interfaces/test_interface.go
+++ backend/interfaces/test_interface.go
@@ -1,27 +1,10 @@
 package interfaces
 
-import (
-	"career-paths/dto"
-	"career-paths/entities"
-	"career-paths/utils"
-
-	"github.com/labstack/echo/v4"
-)
-
 type TestRepository interface {
-	CreateTestRepository(test *dto.CreateQuestion) error
-	GetTestRepository(id string) (*entities.Test, error)
-	GetAllTestRepository() ([]dto.FindAllQuestionResponse, error)
 }
 
 type TestService interface {
-	CreateTestService(data *dto.CreateQuestion) *utils.Response
-	GetTestService(id string) (*entities.Test, *utils.Response)
-	GetAllTestService() ([]dto.FindAllQuestionResponse, *utils.Response)
 }
 
 type TestHandler interface {
-	CreateTest(c echo.Context) error
-	GetTest(c echo.Context) error
-	GetAllTest(c echo.Context) error
 }
